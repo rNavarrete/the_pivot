227f2661a846f22488a90f8e57369f5d437b8d4e
awortham <awortham@newlifechurch.tv>
2014-09-29 16:24:26 -0600

    show mentor

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,18 @@
 class CategoriesController < ApplicationController
+  def index
+    @category = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def create
+    @category = Category.create(category_params)
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name, :description)
+  end
 end
