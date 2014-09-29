4631429e824d9204e74e2980aabbecf9ad2edd9f
awortham <awortham@newlifechurch.tv>
2014-09-29 11:19:45 -0600

    wrote some tests for category

---
+++ spec/features/create_category_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :feature do
+  it 'can create a category from the user face' do
+    visit categories_path
+    click_link 'Create'
+
+    expect(current_path).to eq(new_category_path)
+
+    fill_in 'Name', with: 'Deserts'
+    clink_button 'Create Category'
+    expect(current_path).to eq(category_path(Category.last))
+    expect(page).to have_text('Category Successfuly Created!')
+    expect(page).to have_text('New Category Title')
+  end
+end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Category, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'create categories' do
+    it 'can create a category' do
+      category = Category.create(name: "Soups", description: "delicious")
+      expect(category.description).to eq('delicious')
+    end
+
+    it 'will not create a category if it does not have a name' do
+      category = Category.new(description: 'yummy')
+      expect(category.valid?).to eq(false)
+    end
+  end
 end
