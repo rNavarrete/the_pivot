9bc930de565329c08eddc74e3ea10fa3403bc613
awortham <awortham@newlifechurch.tv>
2014-09-29 21:13:16 -0600

    crud for items, need tests

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,39 @@
 class Admin::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to admin_items_path
+  end
+
+  def create
+    @item = Item.create(item_params)
+    redirect_to admin_items_path
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update_attributes(item_params)
+    redirect_to admin_items_path
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
 end
