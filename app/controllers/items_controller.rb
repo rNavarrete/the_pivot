d87194bb9e94ba0cd531e1469afd4a7851aefd00
awortham <awortham@newlifechurch.tv>
2014-10-02 15:23:51 -0600

    retire item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,6 @@
 class ItemsController < ApplicationController
 
   def index
-    
+
   end
- # def current_user
- #   @user = User.find_by(email_address: params[:email_address])
- # end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,16 @@
 class Item < ActiveRecord::Base
-  has_many :category_items
-  has_many :categories, through: :category_items
+  has_many  :category_items
+  has_many  :categories, through: :category_items
+
   validates :name, presence: :true
   validates :description, length: { minimum: 25 }
   validates :price, presence: :true
   validates :price, numericality: { greater_than_or_equal_to: 0}
+  validates :status, inclusion: ['active', 'retired', 'Active', 'Retired']
 
     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  def self.active
+    where(status: 'active')
+  end
 end
