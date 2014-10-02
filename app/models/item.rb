08dac84640730462665e2f74d66610a5817b3a73
awortham <awortham@newlifechurch.tv>
2014-10-02 16:26:05 -0600

    finished retiring item

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,9 +8,13 @@ class Item < ActiveRecord::Base
   validates :price, numericality: { greater_than_or_equal_to: 0}
   validates :status, inclusion: ['active', 'retired', 'Active', 'Retired']
 
-    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   def self.active
     where(status: 'active')
+  end
+
+  def status=(new_status)
+    super(new_status.downcase)
   end
 end
