fbf98b9c5b399a8953c4dc26cca2f0f80cd7678f
Glen Egbert <glenegbert1@gmail.com>
2014-10-02 07:39:52 -0600

    fixed some tests, need to work on design before fixing the rest

--- spec/features/admin_view_spec.rb
+++ spec/features/admin_view_spec.rb
@@ -3,34 +3,31 @@ require 'capybara/rails'
 require 'capybara/rspec'
 
 describe 'the admin view', type: :feature do
-
-  # describe 'authentication', type: :feature do
-  #
-  #   it 'can see dashboard' do
-  #     visit items_path
-  #     expect(page).to_not have_link('Admin Dashboard')
-  #     user = user_with({})
-  #     user.save
-  #     page.fill_in('Email address', with: 'admin@example.com')
-  #     page.fill_in('Password', with: '1234')
-  #     page.click_button('Log In')
-  #     expect(page).to have_link('Admin Dashboard')
-  #     click_link 'Admin Dashboard'
-  #     expect(page).to have_css('#dashboard_heading')
-
   describe 'admin tasks', type: :feature do
-    User.create(full_name: "John", email_address: 'admin@example.com', password: '1234', password_confirmation: "1234", role: 'admin')
+    it 'can see dashboard' do
+      visit items_path
+      expect(page).to_not have_link('Admin Dashboard')
+      user = user_with({})
+      user.save
+      page.fill_in('Email address', with: 'admin@example.com')
+      page.fill_in('Password', with: '1234')
+      page.click_button('Log In')
+      expect(page).to have_link('Admin Dashboard')
+      click_link 'Admin Dashboard'
+      expect(page).to have_css('#dashboard_heading')
+    end
 
-      it 'can see dashboard' do
-        visit items_path
-        expect(page).to_not have_link('Admin Dashboard')
-        page.fill_in('Email address', with: 'admin@example.com')
-        page.fill_in('Password', with: '1234')
-        page.click_button('Log In')
-        expect(page).to have_link('Admin Dashboard')
-        click_link 'Admin Dashboard'
-        expect(page).to have_css('#dashboard_heading')
-      end
+
+      # it 'can see dashboard' do
+      #   visit items_path
+      #   expect(page).to_not have_link('Admin Dashboard')
+      #   page.fill_in('Email address', with: 'admin@example.com')
+      #   page.fill_in('Password', with: '1234')
+      #   page.click_button('Log In')
+      #   expect(page).to have_link('Admin Dashboard')
+      #   click_link 'Admin Dashboard'
+      #   expect(page).to have_css('#dashboard_heading')
+      # end
 
       it 'can modify item name =' do
         category_1 = Category.create(name: "Desserts")
@@ -146,6 +143,7 @@ describe 'the admin view', type: :feature do
 
   it 'can create and edit a category from the admin face' do
     user = user_with({})
+    user.save
     login_as user
 
     visit admin_dashboard_path
@@ -166,6 +164,10 @@ describe 'the admin view', type: :feature do
   end
 
   it 'can create a category from the admin face' do
+    user = user_with({})
+    user.save
+    login_as(user)
+
     visit admin_categories_path
     click_link 'Create'
 
@@ -177,29 +179,31 @@ describe 'the admin view', type: :feature do
     expect(page).to have_css('#listings')
   end
 
-  it 'can create/edit/delete an item when logged in as an admin' do
+  it 'can create/delete an item when logged in as an admin' do
     user = user_with({})
     user.save
     login_as(user)
+
     visit items_path
     click_link 'Admin Dashboard'
     expect(page).to have_css('#dashboard')
+
+    category_1 = Category.create(name: "Desserts")
+    category_2 = Category.create(name: "Entres")
 
     click_link('Create a new Dish')
     fill_in 'Name', with: "Hamburger"
     fill_in 'Description', with: 'Yum Yum Yum Yum Yum Yum Yum Yum Yum'
     fill_in 'Price', with: '6.5'
+    check('Entres')
     click_button 'Save'
+    click_link 'Entres'
+    save_and_open_page
     expect(page).to have_content('Hamburger')
 
     click_link('Hamburger')
-    fill_in 'Name', with: 'HamBurger and Fries'
-    click_button 'Save'
-    expect(page).to have_content('HamBurger and Fries')
-
-    click_link('HamBurger and Fries')
     click_link('Delete this item')
-    expect(page).to_not have_text('HamBurger and Fries')
+    expect(page).to_not have_text('Hamburger')
   end
 
 end
