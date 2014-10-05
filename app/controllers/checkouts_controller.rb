24386395b5bdc81ec99f405b6f115c2f207007cc
Glen Egbert <glenegbert1@gmail.com>
2014-10-05 06:44:11 -0600

    fixed the multiple orders problem

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -2,8 +2,11 @@ class CheckoutsController < ApplicationController
   layout "special_layout"
 
   def show
-  @order = Order.create(user_id: session[:id])
-  @order.order_items = session[:cart].map { |item_id, quantity| OrderItem.new(item_id: item_id, quantity: quantity, order_id: @order)}
-
+    if Order.last.status == nil
+      @order = Order.last
+    else
+      @order = Order.create(user_id: session[:id])
+      @order.order_items = session[:cart].map { |item_id, quantity| OrderItem.new(item_id: item_id, quantity: quantity, order_id: @order)}
+    end
   end
 end
