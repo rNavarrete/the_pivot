0d160859debe9c41aa88ae89428ad098f6afcbb2
Glen Egbert <glenegbert1@gmail.com>
2014-10-05 19:08:19 -0600

    Checkout table working with subtotal, tax, total

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -4,6 +4,7 @@ class CheckoutsController < ApplicationController
   def show
     if Order.last.status == nil
       @order = Order.last
+      @order.order_items = session[:cart].map { |item_id, quantity| OrderItem.new(item_id: item_id, quantity: quantity, order_id: @order)}
     else
       @order = Order.create(user_id: session[:id])
       @order.order_items = session[:cart].map { |item_id, quantity| OrderItem.new(item_id: item_id, quantity: quantity, order_id: @order)}
