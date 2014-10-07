3ecbef7ff36f1959d7ce778a8d509275c25ad315
Kavita <okavita@gmail.com>
2014-10-07 09:53:39 -0600

    redirect from cart to create account page if need login

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -24,8 +24,8 @@ class CheckoutsController < ApplicationController
 
   def authorize
     unless !!current_user
-      flash[:error] = "You must be logged in to access this section"
-      redirect_to root_path
+      # flash[:error] = "You must be logged in to access this section"
+      redirect_to new_user_path
     end
   end
 
