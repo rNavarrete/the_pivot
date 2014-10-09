3f31e4519ed0d908c87f96bcf5430bbbc45947c2
awortham <awortham@newlifechurch.tv>
2014-10-09 09:23:54 -0600

    links to filters and all orders

--- spec/features/admin/orders/managing_orders_spec.rb
+++ spec/features/admin/orders/managing_orders_spec.rb
@@ -70,5 +70,57 @@ feature "Managing Orders" do
       visit admin_orders_path
       expect(page).to have_link "Paid (0)"
     end
+
+    scenario "viewing the completed filter with a completed order" do
+      Order.create(status: 'completed', user_id: user.id)
+      visit admin_orders_path
+      expect(page).to have_link "Completed (1)"
+    end
+
+    scenario "viewing the completed filter with a completed order" do
+      visit admin_orders_path
+      expect(page).to have_link "Completed (0)"
+    end
+
+    scenario "viewing the canceled filter with a canceled order" do
+      Order.create(status: 'canceled', user_id: user.id)
+      visit admin_orders_path
+      expect(page).to have_link "Canceled (1)"
+    end
+
+    scenario "viewing the canceled filter with a canceled order" do
+      visit admin_orders_path
+      expect(page).to have_link "Canceled (0)"
+    end
+
+    scenario "viewing the ordered filter with a ordered order" do
+      Order.create(status: 'ordered', user_id: user.id)
+      visit admin_orders_path
+      expect(page).to have_link "Ordered (1)"
+    end
+
+    scenario "viewing the ordered filter with a ordered order" do
+      visit admin_orders_path
+      expect(page).to have_link "Ordered (0)"
+    end
+
+    scenario "clicking all will allow you to view page with all orders again" do
+      Order.create(status: 'ordered', user_id: user.id)
+      Order.create(status: 'canceled', user_id: user.id)
+      Order.create(status: 'ordered', user_id: user.id)
+      Order.create(status: 'paid', user_id: user.id)
+      visit admin_orders_path
+      expect(page).to have_link "Ordered (2)"
+      expect(page).to have_link "Paid (1)"
+      expect(page).to have_link "Canceled (1)"
+
+      click_link('Ordered (2)')
+
+      expect(page).to have_text "13 - John"
+      click_link('View All Orders')
+      expect(page).to have_text "14 - John"
+
+
+    end
   end
 end
