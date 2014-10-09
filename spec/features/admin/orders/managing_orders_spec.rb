2cab9b6bcf0edd49bef417f2558565ea7d21ddd4
awortham <awortham@newlifechurch.tv>
2014-10-09 10:35:13 -0600

    tests for orders

--- spec/features/admin/orders/managing_orders_spec.rb
+++ spec/features/admin/orders/managing_orders_spec.rb
@@ -106,21 +106,24 @@ feature "Managing Orders" do
 
     scenario "clicking all will allow you to view page with all orders again" do
       Order.create(status: 'ordered', user_id: user.id)
-      Order.create(status: 'canceled', user_id: user.id)
       Order.create(status: 'ordered', user_id: user.id)
-      Order.create(status: 'paid', user_id: user.id)
+      user2 = User.create(full_name: 'Jane', email_address: 'jane@example.com', password: '1234', password_confirmation: '1234', role: 'admin')
+
+      Order.create(status: 'canceled', user_id: user2.id)
+      Order.create(status: 'paid',     user_id: user2.id)
       visit admin_orders_path
+
       expect(page).to have_link "Ordered (2)"
       expect(page).to have_link "Paid (1)"
       expect(page).to have_link "Canceled (1)"
 
       click_link('Ordered (2)')
 
-      expect(page).to have_text "13 - John"
+      expect(page).to have_text "John"
+      expect(page).to_not have_text "Jane"
       click_link('View All Orders')
-      expect(page).to have_text "14 - John"
-
-
+      expect(page).to have_text "John"
+      expect(page).to have_text "Jane"
     end
   end
 end
