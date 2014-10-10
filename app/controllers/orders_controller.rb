c19c9ecd32122d57c86a2f28a6b23f3e5cfade41
Glen Egbert <glenegbert1@gmail.com>
2014-10-10 08:12:17 -0600

    made checkout for pick up restful

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,16 +12,27 @@ class OrdersController < ApplicationController
     # # binding.pry
     # order.build_order(session[:cart_items])
     # redirect_to order_path(order)
-    
-    @order = Order.create(user_id: session[:user_id], status: "ordered")
-    @order.order_items = session[:cart_items].map { |item_id, quantity| OrderItem.new(item_id: item_id, quantity: quantity, order_id: @order)}
-    @order.address = params[:address]
-    session[:cart_items] = {}
-    redirect_to verification_path
+
+    if session[:cart_items].count == 0
+      redirect_to verification_path
+    else
+      @order = Order.create(user_id: session[:user_id], status: "ordered")
+      @order.order_items = session[:cart_items].map { |item_id, quantity| OrderItem.new(item_id: item_id, quantity: quantity, order_id: @order)}
+      session[:cart_items] = {}
+      redirect_to verification_path
+    end
   end
 
   def new
+    if cart.empty?
+      redirect_to cart_path, notice: 'Please add items to your cart before checking out. Thank you!'
+    else
+      @subtotal = Checkout.subtotal(session)
+      @tax = Checkout.tax(session)
+      @total = Checkout.total(session)
+    end
   end
+
 
   def show
     @order = Order.find(params[:id])
