12e76e87221f2c5bdc2e4e93743e1d3fce54fb33
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-10-29 14:34:30 -0600

    spacing issues

---
+++ spec/features/validations_and_error_messages_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+require 'capybara/rails'
+require 'capybara/rspec'
+
+
+
+describe 'Forms in the application must recognize invalid entries', type: :feature do
+
+ it 'must reject invalid input'
+
+ it 'display clear and helpful errors and corrective instructions'
+
+ it 'allow the user to quickly fix and resubmit'
+end
