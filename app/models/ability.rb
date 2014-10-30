472c94c91527f5693b19b946f7cd6d9e4b32712b
Ian Andersen <ianderse@mac.com>
2014-10-30 14:44:08 -0600

    comments in ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,7 @@ class Ability
       if user.is? :admin
         can :manage, :all
       elsif user.is? :seller
-        can :manage, Store
+        can :manage, Store #need to check that seller owns the store still
       else
         can :read, :all
       end
