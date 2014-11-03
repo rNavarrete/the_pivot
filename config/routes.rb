a56c2de6cab87b22b0de0abbde983450cb90604e
Ian Andersen <ianderse@mac.com>
2014-11-03 11:24:49 -0700

    further clean up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,27 @@
 Rails.application.routes.draw do
 
+  root 'home#index'
+
   resources :items
-
 	resources :orders
-
   resources :categories
-
   resources :addresses
+  resources :users
+  resources :home
+
+  namespace :admin do
+    resources :items, :categories, :orders, :order_items
+    get 'dashboard', to: 'dashboard#show'
+  end
+
+  get ':id', to: 'stores#show', as: 'stores'
 
   get 'confirmation', to: 'confirmations#show'
 
   get 'code', to: 'home#code'
 
   get 'complete_delivery_order', to: 'orders#create'
+
   get 'checkout',         to: 'checkouts#show'
   get 'complete_order',   to: 'checkouts#complete_order'
 
@@ -21,29 +30,15 @@ Rails.application.routes.draw do
   get 'remove_item',      to: 'carts#remove_item',    as: 'remove_item'
   put 'update_quantity',  to: 'carts#update_quantity',as: 'update_quantity'
 
-  namespace :admin do
-    resources :items, :categories, :orders, :order_items
-    get 'dashboard', to: 'dashboard#show'
-  end
-
   get 'stores', to: 'store_dashboard#index', as: 'store_dashboard'
   get 'stores/:store_name', to: 'store_dashboard#show', as: 'store_dashboard_show'
-
-  resources :users
-
-  get ':id', to: 'stores#show', as: 'stores'
 
   get    'login',  to: 'sessions#new'
   post   'login',  to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  root 'home#index'
-
-  resources :home
-
-  get 'gift_cards', to: 'gift_cards#show'
-  get 'about',      to: 'about#show'
-  match '/contact_us',     to: 'contact_us#new',             via: 'get'
+  get 'gift_cards',        to: 'gift_cards#show'
+  get 'about',             to: 'about#show'
   resources "contact_us", only: [:new, :create]
 
 end
