34b766d9187b0eda2d364b41b4b1817058b51f52
Sara Simon <smbsimon@gmail.com>
2014-11-04 13:44:10 -0700

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,7 +2,7 @@ class Address < ActiveRecord::Base
   has_many :orders
   belongs_to :user
   validates :street_address, presence: :true
-  validates :city, presence: :true, format: { with: /\D/ }
+  validates :city, presence: :true, format: { with: /\A\D*\z/ }
   validates :state, length: {is: 2}, format: { with: /\A[a-zA-Z]+\z/ }
   validates :zip, presence: :true, numericality: :true
 
