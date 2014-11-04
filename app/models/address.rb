639a00c37a4e9ddb04d09dd377164fa08a90ab47
awortham <awortham@newlifechurch.tv>
2014-11-04 13:47:02 -0700

    fixed city validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,10 +2,7 @@ class Address < ActiveRecord::Base
   has_many :orders
   belongs_to :user
   validates :street_address, presence: :true
-  validates :city, presence: :true, format: { with: /\D/ }
+  validates :city, presence: :true, format: { with: /\A[a-z A-Z]+\z/ }
   validates :state, length: {is: 2}, format: { with: /\A[a-zA-Z]+\z/ }
   validates :zip, presence: :true, numericality: :true
-
-
-
 end
