7f865d60f423ad83613a82db90367f8f61d2c6ad
Glen Egbert <glenegbert1@gmail.com>
2014-11-06 05:39:11 -0700

    added user can edit account details page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,19 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def edit
+		@user = current_user
+	end
+
+	def update
+		@user = current_user
+		if @user.update(user_params)
+			redirect_to '/', notice: 'Information Successfully Updated!'
+		else
+			render :edit
+		end
+	end
+
 	private
 
 	def user_params
