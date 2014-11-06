666e14f5dd1190da14e14b9a200b38ee3c9f033b
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-11-05 20:03:34 -0700

    merged in ians last commit from master

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105181743) do
+ActiveRecord::Schema.define(version: 20141105225144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 20141105181743) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "slug"
+    t.boolean  "authorized",         default: false
   end
 
   add_index "stores", ["user_id"], name: "index_stores_on_user_id", using: :btree
