e6c55367f2594c5e6660cfc243eb2b63881d22fd
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-11-11 16:21:19 -0700

    added store manager validation

--- app/controllers/seller/seller_controller.rb
+++ app/controllers/seller/seller_controller.rb
@@ -36,8 +36,18 @@ class Seller::SellerController < ApplicationController
 
 	def add_manager
 		@store = Store.find(params[:id])
-		@store.users << User.where(:email_address => params[:email]).first
-		raise "OMG"
+		user = User.where(:email_address => params[:email]).first
+		if @store.users.include?(user)
+			redirect_to :back
+			flash[:notice] = "User is already a manager."
+		elsif User.all.include?(user)
+			@store.users << User.where(:email_address => params[:email]).first
+			redirect_to seller_dashboard_path
+			flash[:notice] = "Successfully added a manager to your store."
+		else
+			redirect_to :back
+			flash[:notice] = "Unable to find this user"
+		end
 	end
 
 	private
@@ -48,7 +58,8 @@ class Seller::SellerController < ApplicationController
 
 	def store_owner?(store)
 		stores = Store.where(:user_id => current_user.id)
-		if stores.include?(store) or current_user.is? :admin
+		store_managers = store.users
+		if stores.include?(store) or current_user.is? :admin or store_managers.include?(current_user)
 		else
     	redirect_to root_path
     	flash[:notice] = "This isn't your store."
