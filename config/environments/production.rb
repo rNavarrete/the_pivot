5107c071dc6ed27ea69bae2d8e51d479a61cf6ac
Ian Andersen <ianderse@mac.com>
2014-11-11 13:23:08 -0700

    fix stupid bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,14 +66,15 @@ config.paperclip_defaults = {
  #   password:             ENV['GMAIL_PASSWORD'],
  #   authentication:       'plain',
  #   enable_starttls_auto: true  }
- # end
 
- ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'yourdomain.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+
+   ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'yourdomain.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+ end
