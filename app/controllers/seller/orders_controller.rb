94ada5cc534d5b79614a2ba514a4146c58699457
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-11-12 11:18:36 -0700

    fixed orders updating paths or something

--- app/controllers/seller/orders_controller.rb
+++ app/controllers/seller/orders_controller.rb
@@ -6,7 +6,7 @@ class Seller::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
-    redirect_to seller_dashboard_path, notice: "Order Successfully Updated"
+    redirect_to seller_store_edit_path(@order.store.id), notice: "Order Successfully Updated"
   end
 
   private
